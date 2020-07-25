de867255e4d0f54b5ff6c0d938ffe5a27d22f532
Ryan Ling <ryan@outlook.com.au>
2020-07-25 18:00:20 +1000

    fix: Clean up old styles

--- src/private/useMdxComponents.treat.ts
+++ src/private/useMdxComponents.treat.ts
@@ -1,9 +1,7 @@
 import { darken } from 'polished';
-import { Style, style, styleMap } from 'sku/treat';
+import { style } from 'sku/treat';
 
 import { monospaceFontFamily } from '../styles';
-
-import { SIZES, SIZE_TO_PADDING, SIZE_TO_SPACE, Size } from './size';
 
 export const inlineCode = style((theme) => ({
   backgroundColor: theme.color.background.body,
@@ -14,46 +12,6 @@ export const inlineCode = style((theme) => ({
   fontFamily: monospaceFontFamily,
   fontSize: '0.9em',
 }));
-
-export const listGrid = styleMap<Size>((theme) =>
-  SIZES.reduce<Record<Size, Style>>((acc, size) => {
-    acc[size] = {
-      display: 'grid',
-      gridColumnGap: theme.grid * theme.space[SIZE_TO_PADDING[size]],
-      gridRowGap: theme.grid * theme.space[SIZE_TO_SPACE[size]],
-      gridTemplateColumns: 'min-content minmax(0, 1fr)',
-    };
-
-    return acc;
-  }, {} as Record<Size, Style>),
-);
-
-export const orderedList = style({
-  counterReset: 'ol',
-});
-
-export const unorderedList = style({
-  counterReset: 'ol',
-});
-
-export const listItem = style({
-  display: 'list-item',
-  textAlign: 'right',
-  selectors: {
-    [`${orderedList} > span > &, ${orderedList} > div > div > span > &:before`]: {
-      counterIncrement: 'ol',
-    },
-    [`${orderedList} > span > &:before, ${orderedList} > div > div > span > &:before`]: {
-      content: "counter(ol) '.'",
-    },
-    [`${unorderedList} > span > &:before, ${unorderedList} > div > div > span > &:before`]: {
-      content: "'•'",
-    },
-    [`${unorderedList} > span > &, ${unorderedList} > div > div > span > &`]: {
-      transform: 'scale(1.25)',
-    },
-  },
-});
 
 export const pre = style({
   margin: 0,
