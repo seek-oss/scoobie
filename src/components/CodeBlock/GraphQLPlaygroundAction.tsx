8899ee8deaa5f52d87f5ecec14001424c87e3b53
Ryan Ling <ryan@outlook.com.au>
2020-08-24 08:05:41 +1000

    fix(CodeBlock): Fix URL ReferenceError
    Sigh.

--- src/components/CodeBlock/GraphQLPlaygroundAction.tsx
+++ src/components/CodeBlock/GraphQLPlaygroundAction.tsx
@@ -1,7 +1,11 @@
+import url from 'url';
+
 import { IconVideo, Text, TextLink } from 'braid-design-system';
 import React from 'react';
 
 import { SIZE_TO_CODE_SIZE, Size } from '../../private/size';
+
+const URL = url.URL ?? window.URL;
 
 interface Props {
   children: string;
@@ -14,14 +18,15 @@ export const GraphQLPlaygroundAction = ({
   graphqlPlayground,
   size,
 }: Props) => {
-  const url = new URL(graphqlPlayground);
-  url.searchParams.set('query', children);
+  const playgroundUrl = new URL(graphqlPlayground);
+  playgroundUrl.searchParams.set('query', children);
+  const href = playgroundUrl.toString();
 
   const codeSize = SIZE_TO_CODE_SIZE[size];
 
   return (
     <Text size={codeSize} weight="medium">
-      <TextLink href={url.toString()} rel="noreferrer" target="_blank">
+      <TextLink href={href} rel="noreferrer" target="_blank">
         <IconVideo alignY="lowercase" /> Playground
       </TextLink>
     </Text>
