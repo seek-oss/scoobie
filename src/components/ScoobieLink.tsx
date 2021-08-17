d0c837c1e8480aed84acf90e1d83d06c9f5c5480
Raphael Suttiyotin <rsuttiyotin@seek.com.au>
2021-08-17 15:40:05 +1000

    feat(ScoobieLink): Render download links as anchors (#277)
    feat(ScoobieLink): Render download links as anchors
    
    Co-authored-by: Ryan Ling <ryan@outlook.com.au>

--- src/components/ScoobieLink.tsx
+++ src/components/ScoobieLink.tsx
@@ -6,14 +6,34 @@ import { isExternalHref } from '../private/url';
 import { InternalLink } from './InternalLink';
 
 export const ScoobieLink = makeLinkComponent(
-  ({ children, href, ...restProps }, ref) =>
-    isExternalHref(href) ? (
-      <a rel="noreferrer" target="_blank" {...restProps} href={href} ref={ref}>
-        {children}
-      </a>
-    ) : (
+  ({ children, href, download, ...restProps }, ref) => {
+    if (isExternalHref(href)) {
+      return (
+        <a
+          rel="noreferrer"
+          target="_blank"
+          {...restProps}
+          download={download}
+          href={href}
+          ref={ref}
+        >
+          {children}
+        </a>
+      );
+    }
+
+    if (download) {
+      return (
+        <a {...restProps} href={href} ref={ref} download={download}>
+          {children}
+        </a>
+      );
+    }
+
+    return (
       <InternalLink {...restProps} href={href} innerRef={ref} reset={false}>
         {children}
       </InternalLink>
-    ),
+    );
+  },
 );
