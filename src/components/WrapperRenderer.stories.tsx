615fdf8cd7c8f1d3b6cd714f6fe206ff8db9dbd0
Ryan Ling <ryan@outlook.com.au>
2021-08-18 23:45:04 +1000

    Migrate to Component Story Format
    https://storybook.js.org/docs/react/api/csf

--- src/components/WrapperRenderer.stories.tsx
+++ src/components/WrapperRenderer.stories.tsx
@@ -3,19 +3,21 @@ import 'loki/configure-react';
 
 import { Text } from 'braid-design-system';
 import React from 'react';
-import { storiesOf } from 'sku/@storybook/react';
 
-import { withDecorator } from '../storybook/decorator';
+import { ScoobieDecorator } from '../storybook/decorator';
 import Wrapper from '../storybook/markdown/wrapper.mdx';
 
 import { WrapperRenderer } from './WrapperRenderer';
 
-storiesOf('WrapperRenderer', module)
-  .addDecorator(withDecorator)
-  .add('Example', () => (
-    <WrapperRenderer document={Wrapper}>
-      {({ children }) => (
-        <Text>{React.Children.toArray(children).length} top-level node(s)</Text>
-      )}
-    </WrapperRenderer>
-  ));
+export default {
+  decorators: [ScoobieDecorator],
+  title: 'WrapperRenderer',
+};
+
+export const Example = () => (
+  <WrapperRenderer document={Wrapper}>
+    {({ children }) => (
+      <Text>{React.Children.toArray(children).length} top-level node(s)</Text>
+    )}
+  </WrapperRenderer>
+);
