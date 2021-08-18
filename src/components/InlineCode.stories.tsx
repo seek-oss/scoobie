2b3d2b7789f26ca780e4c3ab7c476bfce75c16ca
Ryan Ling <ryan@outlook.com.au>
2021-08-19 08:37:30 +1000

    fix(pkg): Support sku 11 (#286)
    This is a patch release as it widens our peer dependency range.
    
    - https://github.com/seek-oss/sku/releases/tag/v11.0.0
    - https://github.com/seek-oss/sku/releases/tag/v11.0.1

--- src/components/InlineCode.stories.tsx
+++ src/components/InlineCode.stories.tsx
@@ -1,9 +1,9 @@
 import 'braid-design-system/reset';
 import 'loki/configure-react';
 
+import { select, text } from '@storybook/addon-knobs';
 import { Text } from 'braid-design-system';
 import React from 'react';
-import { select, text } from 'sku/@storybook/addon-knobs';
 import { storiesOf } from 'sku/@storybook/react';
 
 import { withDecorator } from '../storybook/decorator';
--- src/components/InternalLink.stories.tsx
+++ src/components/InternalLink.stories.tsx
@@ -1,9 +1,9 @@
 import 'braid-design-system/reset';
 import 'loki/configure-react';
 
+import { boolean, text } from '@storybook/addon-knobs';
 import { Alert, Stack, Text } from 'braid-design-system';
 import React from 'react';
-import { boolean, text } from 'sku/@storybook/addon-knobs';
 import { storiesOf } from 'sku/@storybook/react';
 
 import { withDecorator } from '../storybook/decorator';
