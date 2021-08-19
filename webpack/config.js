dc6586331aa55e53447958e6e06b182e0363d2b5
Ryan Ling <ryan@outlook.com.au>
2021-08-20 08:50:11 +1000

    fix(webpack): Override sku image loaders (#297)
    These aren't playing nicely with our MDX pipeline. Hopefully I can
    fast-follow with a migration to Asset Modules:
    
    https://webpack.js.org/guides/asset-modules/
    
    I've also bumped a bunch of caret ranges while I'm here, and cleaned up
    our Webpack config merging with `webpack-merge` (closes #20).

--- webpack/config.js
+++ webpack/config.js
@@ -1,12 +1,11 @@
+const { merge } = require('webpack-merge');
+
 const { ScoobieWebpackPlugin } = require('./webpackPlugin');
 
-module.exports.dangerouslySetWebpackConfig = (webpackConfig) => ({
-  ...webpackConfig,
-
-  plugins: [
-    ...webpackConfig.plugins,
-
-    // Run after SkuWebpackPlugin
-    new ScoobieWebpackPlugin(),
-  ],
-});
+module.exports.dangerouslySetWebpackConfig = (webpackConfig) =>
+  merge(webpackConfig, {
+    plugins: [
+      // This must run after SkuWebpackPlugin.
+      new ScoobieWebpackPlugin(),
+    ],
+  });
