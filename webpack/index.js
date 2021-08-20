34f20a980c91fea679ee4320351b6a2f9ce9f329
Ryan Ling <ryan@outlook.com.au>
2021-08-20 12:27:39 +1000

    fix(webpack): Re-export merge function (#300)

--- webpack/index.js
+++ webpack/index.js
@@ -1,7 +1,10 @@
+const { merge } = require('webpack-merge');
+
 const { dangerouslySetWebpackConfig } = require('./config');
 const { ScoobieWebpackPlugin } = require('./webpackPlugin');
 
 module.exports = {
   ScoobieWebpackPlugin,
   dangerouslySetWebpackConfig,
+  merge,
 };
