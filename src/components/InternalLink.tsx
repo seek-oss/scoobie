4460131f8a34b822dfe358af343999bbb93f75c2
Ryan Ling <ryan@outlook.com.au>
2021-11-05 08:48:43 +1100

    Start on forwarding ref

--- src/components/InternalLink.tsx
+++ src/components/InternalLink.tsx
@@ -1,5 +1,5 @@
 import clsx, { ClassValue } from 'clsx';
-import React, { ComponentProps } from 'react';
+import React, { ComponentProps, forwardRef } from 'react';
 import { useLocation } from 'react-router-dom';
 import { NavHashLink } from 'react-router-hash-link';
 
@@ -18,39 +18,37 @@ interface Props
   state?: any;
 }
 
-export const InternalLink = ({
-  className,
-  href,
-  reset = true,
-  state,
-  ...restProps
-}: Props) => {
-  const scroll = (element: Element) =>
-    setTimeout(() => {
-      // Scroll to the header's `Stack` element so we don't cut off the heading
-      (element.parentElement ?? element).scrollIntoView({
-        behavior: 'smooth',
+export const InternalLink = forwardRef(
+  ({ className, href, reset = true, state, ...restProps }: Props, ref) => {
+    const scroll = (element: Element) =>
+      setTimeout(() => {
+        // Scroll to the header's `Stack` element so we don't cut off the heading
+        (element.parentElement ?? element).scrollIntoView({
+          behavior: 'smooth',
+        });
       });
-    });
 
-  const location = useLocation();
+    const location = useLocation();
 
-  const to = { ...parseInternalHref(href, location), state };
+    const to = { ...parseInternalHref(href, location), state };
 
-  return (
-    <NavHashLink
-      {...restProps}
-      className={
-        // TODO: DefinitelyTyped/DefinitelyTyped#56976
-        (((isActive: boolean) =>
-          clsx(
-            reset ? styles.reset : null,
-            typeof className === 'function' ? className(isActive) : className,
-          )) as unknown) as string
-      }
-      scroll={scroll}
-      smooth
-      to={to}
-    />
-  );
-};
+    return (
+      <NavHashLink
+        {...restProps}
+        className={
+          // TODO: DefinitelyTyped/DefinitelyTyped#56976
+          (((isActive: boolean) =>
+            clsx(
+              reset ? styles.reset : null,
+              typeof className === 'function' ? className(isActive) : className,
+            )) as unknown) as string
+        }
+        // TODO: verify if this actually works as intended
+        ref={ref as any}
+        scroll={scroll}
+        smooth
+        to={to}
+      />
+    );
+  },
+);
