b1697adc70d4a28ef23bf28355444a82be3619c7
Ryan Ling <ryan@outlook.com.au>
2021-11-24 15:55:14 +1100

    feat(typography): Load web fonts asynchronously (#352)
    Per https://web.dev/defer-non-critical-css/#optimize.

--- typography.ts
+++ typography.ts
@@ -4,6 +4,12 @@ export const robotoHref =
 export const robotoMonoHref =
   'https://fonts.googleapis.com/css2?family=Roboto+Mono:ital,wght@0,400;0,500;0,700;1,400;1,500;1,700&display=swap';
 
-export const robotoHtml = `<link href="${robotoHref}" rel="stylesheet">`;
+export const robotoHtml = `
+<link rel="preload" href="${robotoHref}" as="style" onload="this.onload=null;this.rel='stylesheet'">
+<noscript><link rel="stylesheet" href="${robotoHref}"></noscript>
+`.trim();
 
-export const robotoMonoHtml = `<link href="${robotoMonoHref}" rel="stylesheet">`;
+export const robotoMonoHtml = `
+<link rel="preload" href="${robotoMonoHref}" as="style" onload="this.onload=null;this.rel='stylesheet'">
+<noscript><link rel="stylesheet" href="${robotoMonoHref}"></noscript>
+`.trim();
