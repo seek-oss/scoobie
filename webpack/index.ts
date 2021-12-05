f5cf53a9e9ae9f9a8b135fdab952b12e67d8bbd1
Ryan Cumming <etaoins@gmail.com>
2021-12-06 07:22:59 +1100

    fix: Switch Webpack plugins back to JavaScript (#357)
    * fix: Switch Webpack plugins back to JavaScript
    
    This reverts most of #354. We're included cross-package from other Sku
    config so we knew this would break if included from an older version of
    Sku that didn't support TypeScript configs.
    
    However, it seems this doesn't work even from TypeScript Sku configs;
    our Webpack plugins get incorrectly parsed as JavaScript.
    
    It's possible this would work if we distributed JavaScript but we're a
    `compilePackage`.
    
    * Switch to `import type`

--- webpack/index.ts
+++
@@ -1,6 +0,0 @@
-import { merge } from 'webpack-merge';
-
-import { dangerouslySetWebpackConfig } from './config';
-import { ScoobieWebpackPlugin } from './webpackPlugin';
-
-export { dangerouslySetWebpackConfig, ScoobieWebpackPlugin, merge };
