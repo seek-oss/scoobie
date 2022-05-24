bc56dad68a199b40328578b81d12bcf164b3f478
Ryan Ling <ryan@outlook.com.au>
2022-05-25 09:42:15 +1000

    fix: Hack in "support" for React 18 typings (#423)

--- src/components/MdxProvider.tsx
+++ src/components/MdxProvider.tsx
@@ -1,4 +1,4 @@
-import { MDXProvider } from '@mdx-js/react';
+import { MDXProvider, MDXProviderComponents } from '@mdx-js/react';
 import React, { ReactNode } from 'react';
 
 import { GraphQLPlaygroundProvider } from '../private/hooks/graphqlPlayground';
@@ -30,7 +30,10 @@ export const MdxProvider = ({
 
   return (
     <GraphQLPlaygroundProvider value={graphqlPlayground}>
-      <MDXProvider components={components}>{children}</MDXProvider>
+      {/* `@types/mdx-js__react` does not play nicely with React 18 */}
+      <MDXProvider components={components as MDXProviderComponents}>
+        {children}
+      </MDXProvider>
     </GraphQLPlaygroundProvider>
   );
 };
