c07c570df3e5b5a56c65548fc29e5799df3f2914
Ryan Ling <ryan@outlook.com.au>
2022-12-07 16:24:10 +1100

    feat(SmartTextLink): Propagate `icon` prop (#468)
    https://seek-oss.github.io/braid-design-system/components/TextLink/releases#v31.21.0

--- src/components/SmartTextLink.tsx
+++ src/components/SmartTextLink.tsx
@@ -1,5 +1,5 @@
 import { TextLink } from 'braid-design-system';
-import React, { ReactNode } from 'react';
+import React, { ComponentProps, ReactNode } from 'react';
 
 import { ExternalTextLink } from '../private/ExternalTextLink';
 import { isExternalHref } from '../private/url';
@@ -7,16 +7,17 @@ import { isExternalHref } from '../private/url';
 interface Props {
   children: ReactNode;
   href: string;
+  icon?: ComponentProps<typeof TextLink>['icon'];
   title?: string;
 }
 
-export const SmartTextLink = ({ children, href, title }: Props) =>
+export const SmartTextLink = ({ children, href, icon, title }: Props) =>
   isExternalHref(href) ? (
-    <ExternalTextLink href={href} title={title}>
+    <ExternalTextLink href={href} icon={icon} title={title}>
       {children}
     </ExternalTextLink>
   ) : (
-    <TextLink href={href} title={title}>
+    <TextLink href={href} icon={icon} title={title}>
       {children}
     </TextLink>
   );
