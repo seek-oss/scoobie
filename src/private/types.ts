7569404e3b46504f1525b878b827b5d1079c260f
Ryan Ling <ryan@outlook.com.au>
2024-10-11 14:50:11 +1100

    Remove more dead code (#738)

--- src/private/types.ts
+++ src/private/types.ts
@@ -1,6 +1,4 @@
 import type { Stack } from 'braid-design-system';
 import type { ComponentProps } from 'react';
 
-export type HeadingLevel = 1 | 2 | 3 | 4 | 5 | 6;
-
 export type StackChildrenProps = Pick<ComponentProps<typeof Stack>, 'children'>;
