092a24b666becfd1e689821ea0bc5a26c3e65ff3
Aaron Moat <2937187+AaronMoat@users.noreply.github.com>
2025-05-30 11:08:07 +1000

    Upgrade storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -13,7 +13,6 @@ export default {
   },
   addons: [
     '@storybook/addon-webpack5-compiler-babel', // Required for Storybook >=8.0.0
-    '@storybook/addon-essentials',
   ],
   babel,
   webpackFinal,
